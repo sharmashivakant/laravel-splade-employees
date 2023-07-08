a1edb9280f310ace0da0147d66bf59fc89684451
sharmashivakant <shivakantsharma918@gmail.com>
2023-07-08 10:42:22 +0530

    full code

---
+++ app/Http/Controllers/PermissionController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tables\Permissions;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use ProtoneMedia\Splade\SpladeForm;
+use ProtoneMedia\Splade\Facades\Splade;
+use Spatie\Permission\Models\Permission;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+use App\Http\Requests\CreatePermissionRequest;
+use App\Http\Requests\UpdatePermissionRequest;
+
+class PermissionController extends Controller
+{
+    public function index()
+    {
+        return view('admin.permissions.index',[
+            'permissions' => Permissions ::class
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.permissions.create',[
+            'roles' => Role::pluck('name','id')->toArray()
+        ]);
+    }
+
+    public function store(CreatePermissionRequest $request, )
+    {
+       $permission = Permission::create($request->validated());
+       $permission->syncRoles($request->roles);
+        Splade::toast('Permission Created successfully')->autoDismiss(3);
+
+        return to_route('admin.permissions.index');
+    }
+
+    public function edit(Permission $permission)
+    {
+        return view('admin.permissions.edit',[
+            'permission' =>$permission,
+            'roles' => Role::pluck('name','id')->toArray()
+        ]);
+    }
+
+     /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePermissionRequest $request, Permission $permission)
+    {
+        $permission->update($request->validated());
+        $permission->syncRoles($request->roles);
+        Splade::toast('Permission Updated successfully')->autoDismiss(3);
+
+        return to_route('admin.permissions.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Permission $permission)
+    {
+        $permission->delete();
+
+        Splade::toast('Permission Deleted successfully')->autoDismiss(3);
+
+        return back();
+    }
+}
---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tables\Roles;
+use App\Tables\Permissions;
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use ProtoneMedia\Splade\SpladeForm;
+use ProtoneMedia\Splade\Facades\Splade;
+use App\Http\Requests\CreateRoleRequest;
+use App\Http\Requests\UpdateRoleRequest;
+use Spatie\Permission\Models\Permission;
+use ProtoneMedia\Splade\FormBuilder\Input;
+use ProtoneMedia\Splade\FormBuilder\Submit;
+
+class RoleController extends Controller
+{
+    public function index()
+    {
+        return view('admin.roles.index',[
+            'roles' => Roles ::class
+        ]);
+    }
+
+    public function create()
+    {
+        
+        return view('admin.roles.create',[
+            'permissions' => Permission::pluck('name','id')->toArray()
+        ]);
+    }
+
+    public function store(CreateRoleRequest $request, )
+    {
+        $role = Role::create($request->validated());
+        $role->syncPermissions($request->permissions);
+        Splade::toast('Role Created successfully')->autoDismiss(3);
+
+        return to_route('admin.roles.index');
+    }
+
+    public function edit(Role $role)
+    {
+       
+
+        return view('admin.roles.edit',[
+            'role' =>$role,
+            'permissions' => Permission::pluck('name','id')->toArray()
+        ]);
+    }
+
+     /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRoleRequest $request, Role $role)
+    {
+        $role->update($request->validated());
+        $role->syncPermissions($request->permissions);
+        Splade::toast('Role Updated successfully')->autoDismiss(3);
+
+        return to_route('admin.roles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Role $role)
+    {
+        $role->delete();
+
+        Splade::toast('Role Deleted successfully')->autoDismiss(3);
+
+        return back();
+    }
+}
